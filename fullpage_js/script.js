5480217149ee69f6c868d77d9e0772020c226383
FlexiLue <felix.luette@web.de>
2021-05-01 01:33:12 +0200

    Inital commit

---
+++ fullpage_js/script.js
@@ -0,0 +1,10 @@
+new fullpage('#fullpage', {
+    autoScrolling: true,
+    scrollBar: false,
+    navigation: true, 
+    navigationPosition: 'right',
+    showActiveTooltip: true,
+    slidesNavigation: true,
+    controlArrows: false,
+    scrollingSpeed: 700,
+  });
---
+++ fullpage_js_simple_demo/script.js
@@ -0,0 +1,11 @@
+new fullpage('#fullpage', {
+  sectionsColor: ['yellow', 'orange', '#C0C0C0'],
+  navigation: true,
+  controlArrows: false,
+  slidesNavigation: true,
+  slidesNavPosition: 'top',
+  autoScrolling: true,
+});
+
+new fullpage('#fullpage', {
+});
